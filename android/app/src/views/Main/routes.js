6829657c4cf5e3ef37c1e9bd202f3f48c824f058
PC-SERLI\mehdi <mehdi.chilla@serli.com>
2017-01-05 11:38:28 +0100

    second commit

---
+++ android/app/src/views/Main/routes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Route, IndexRedirect} from 'react-router'
+import AuthService from 'utils/AuthService'
+import Container from './Container'
+import Home from './Home/Home'
+import Login from './Login/Login'
+
+const auth = new AuthService('HDuEFBgkz3HT8q3o4eVZ9L6Nm5Oquwat', 'yaka.eu.auth0.com');
+
+// validate authentication for private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' })
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+    <Route path="/" component={Container} auth={auth}>
+      <IndexRedirect to="/home" />
+      <Route path="home" component={Home} onEnter={requireAuth} />
+      <Route path="login" component={Login} />
+    </Route>
+  )
+}
+
+export default makeMainRoutes
---
+++ auth0-credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientId: "HDuEFBgkz3HT8q3o4eVZ9L6Nm5Oquwat",
+  domain: "yaka.eu.auth0.com"
+};
